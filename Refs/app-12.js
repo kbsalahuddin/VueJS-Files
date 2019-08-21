56564ea090c9b14f83f11e264442427234a1e533
Khalid Bin Salahuddin <52571806+kbsalahuddin@users.noreply.github.com>
2019-08-21 22:34:41 +0600

    Add files via upload

---
+++ Refs/app-12.js
@@ -0,0 +1,14 @@
+
+new Vue({
+	el:'#vue-app',
+	data:{
+		output:'Your fav food'
+	},
+	methods:{
+		readRefs:function(){
+			
+			this.output=this.$refs.input.value;
+			console.log(this.$refs.test.innerText);
+		}
+	}
+});
